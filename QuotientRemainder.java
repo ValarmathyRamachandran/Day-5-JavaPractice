569d474d29ec9383e133e7510e6ffbefea8a7f61
Valarmathy <valarmathy.ramachandran@outlook.com>
2021-10-04 13:10:51 +0530

    Quotient an dRemainder is calculated for the given num

---
+++ QuotientRemainder.java
@@ -0,0 +1,25 @@
+public class QuotientRemainder{
+	public static void main(String[] args) {
+		//factors
+
+		String commandLineInput1 = "30";
+		int num1 = Integer.parseInt(commandLineInput1);
+				String commandLineInput2 = "10";
+		int num2 = Integer.parseInt(commandLineInput2);
+		
+		int quotient = 0, remainder = 0;  
+  
+      
+    quotient = num1 / num2;  
+  
+    remainder = num1 % num2;  
+  
+    System.out.println("Quotient when " + num1 + "/" + num2 + " is: "   
+  
++ quotient);  
+    System.out.println("Remainder when " + num1 + " is divided by "   
+  
++ num2 + " is: " + remainder);  
+    }  
+}   
+		
